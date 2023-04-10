c0892b90877bb81cfd4a5143dfaf9b625d0841ae
dengt_by dengt_by <denis.wse.new@gmail.com>
2023-04-10 15:24:23 +0300

    Добавьте файлы проекта.

---
+++ CSLightArithmeticsN3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSLightArithmeticsN3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 38;
+            int c = (31 - 5 * a) / b;
+
+            ///<summary>
+            ///
+            ///Последовательность действий
+            ///сначала вычисляется умножение 5х10=50
+            ///далее вычитание 31-50=-19
+            ///далее проводится целочисленное деление без остатка -19/38=0
+            ///в итоге получаем 0
+            ///
+            ///</summary>
+
+            Console.WriteLine(c);
+        }
+    }
+}
